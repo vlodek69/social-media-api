c4b3421cc4dc7012017b3dce0538cf8421aa4ba7
Vlad Larin <lordgadyka71@gmail.com>
2023-12-15 19:12:40 +0200

    DRY social_media/urls.py

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
 from rest_framework import routers
 
 from social_media.views import UserViewSet, PostViewSet, CommentViewSet
 
+
+view_set_dict = {
+    "users": UserViewSet,
+    "posts": PostViewSet,
+    "comments": CommentViewSet,
+}
+
 router = routers.DefaultRouter()
 
-router.register("users", UserViewSet)
-router.register("posts", PostViewSet)
-router.register("comments", CommentViewSet)
+for view_set_prefix, view_set_class in view_set_dict.items():
+    router.register(view_set_prefix, view_set_class)
 
 urlpatterns = router.urls
 
