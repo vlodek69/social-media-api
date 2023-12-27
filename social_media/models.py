58df2161e163ec150c94a01fdcb595c73dc70652
Vlad Larin <lordgadyka71@gmail.com>
2023-12-27 09:27:37 +0200

    Move user fields from Post and Comment models to BasePost

--- social_media/models.py
+++ social_media/models.py
@@ -18,6 +18,11 @@ class BasePost(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=144)
     media = models.ImageField(blank=True, upload_to=post_file_path)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="%(class)s",
+    )
 
     class Meta:
         abstract = True
@@ -28,23 +33,12 @@ class BasePost(models.Model):
 
 
 class Post(BasePost):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="posts",
-    )
-
     @property
     def comments_count(self):
         return self.comments.all().count()
 
 
 class Comment(BasePost):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="comments",
-    )
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
     )
